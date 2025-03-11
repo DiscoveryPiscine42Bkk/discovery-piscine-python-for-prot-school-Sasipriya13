87f6b7f50caa2e62efc181f67cb67787bbdc9532
Sasipriya13 <42912@prot.ac.th>
2025-03-11 11:36:13 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,5 @@
+for num in range(1, 11):
+    print(f"Multiplication table for {num}:")
+    for multiplier in range(1, 11):
+        print(f"{num} x {multiplier} = {num * multiplier}")
+        print("")
